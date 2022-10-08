d19f5a28b7ede1e82b2e5709df6010055f6ca4a7
Maxi1324 <maxi1234567890fischer@gmail.com>
2022-10-08 12:44:45 +0200

    added test project

---
+++ projects/automat-api/src/lib/automat-api.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { AutomatApiComponent } from './automat-api.component';
+
+
+
+@NgModule({
+  declarations: [
+    AutomatApiComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    AutomatApiComponent
+  ]
+})
+export class AutomatApiModule { }
