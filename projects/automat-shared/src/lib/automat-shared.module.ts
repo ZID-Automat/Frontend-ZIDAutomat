5b858a3287d14e2eda5d3487c6cb6ad9c870ba4c
Maximilian Fischer <54480672+Maxi1324@users.noreply.github.com>
2022-10-11 20:55:57 +0200

    Add Packages

--- projects/automat-shared/src/lib/automat-shared.module.ts
+++ projects/automat-shared/src/lib/automat-shared.module.ts
@@ -1,13 +1,23 @@
+import { IconDataService } from './Services/DataServices/icon-data-service';
 import { HttpClientModule, HttpContext } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconComponent } from './Components/Other/icon/icon.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
+    TestComponent,
+    IconComponent,
   ],
   imports: [
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule
   ],
   exports: [
+    TestComponent,
+    IconComponent,
+    FontAwesomeModule
   ],
   providers: [
     HttpContext
