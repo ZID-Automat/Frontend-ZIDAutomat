3b859be09a8d3942b9a6ce7a814887ea56a1c3e5
Maximilian Fischer <54480672+Maxi1324@users.noreply.github.com>
2022-10-14 07:48:34 +0200

    theme

--- projects/automat-shared/src/public-api.ts
+++ projects/automat-shared/src/public-api.ts
@@ -7,3 +7,5 @@ export * from './lib/automat-shared.module';
 export * from './lib/MaterialModule/material.module';
 
 export * from './lib/Components/Other/icon/icon.component'
+
+export * from './lib/Services/DataServices/theme-data.service';
--- projects/user-frontend/src/app/Components/nav-bar/nav-bar.component.ts
+++ projects/user-frontend/src/app/Components/nav-bar/nav-bar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private themeDataService:ThemeDataService) { }
 
   ngOnInit(): void {
   }
