5f6fd54e0415026a1f17c6851a9ac0a13c011ccf
Maxi1324 <maxi1234567890fischer@gmail.com>
2022-10-19 23:47:48 +0200

    TestActiveUserDirectory

--- projects/user-frontend/src/app/Services/user.service.ts
+++ projects/user-frontend/src/app/Services/user.service.ts
@@ -1,5 +1,5 @@
 import { Observable } from 'rxjs';
-import { AccountService, AuthenticationEnum, UserLogin } from 'AutomatApi';
+import { AccountService,AdUser, UserLogin } from 'AutomatApi';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
 export class UserService {
   constructor(private AccountService: AccountService) {}
 
-  login(LoginData:UserLogin) :Observable<AuthenticationEnum>{
+  login(LoginData:UserLogin) :Observable<AdUser>{
     const UserLognOb: UserLogin = { username: LoginData.username, password: LoginData.password };
     return this.AccountService.accountLoginPost$Json({body:UserLognOb});
   }
