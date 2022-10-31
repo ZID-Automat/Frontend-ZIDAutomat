6876283711f61a7301aeed1ea2cc1681f081fc20
Maxi1324 <maxi1234567890fischer@gmail.com>
2022-10-31 17:23:12 +0100

    Added ItemCardDeck

--- projects/user-frontend/src/app/Components/Pages/home-page/item-card-deck/item-card/item-card.component.ts
+++ projects/user-frontend/src/app/Components/Pages/home-page/item-card-deck/item-card/item-card.component.ts
@@ -1,5 +1,5 @@
-import { ThemeDataService } from './../../../../../../../automat-shared/src/lib/Services/DataServices/theme-data.service';
-import { Component, Input, OnInit } from '@angular/core';
+import { ThemeDataService } from '../../../../../../../../automat-shared/src/lib/Services/DataServices/theme-data.service';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { Item, ItemService } from 'projects/user-frontend/src/app/Services/DataServices/item.service';
 
 @Component({
@@ -11,7 +11,7 @@ export class ItemCardComponent implements OnInit {
 
   @Input()Item?:Item ;
 
-  constructor(public ThemeDataService:ThemeDataService) { }
+  constructor(public ThemeDataService:ThemeDataService, public elementRef:ElementRef) { }
 
   ngOnInit(): void {
   }
