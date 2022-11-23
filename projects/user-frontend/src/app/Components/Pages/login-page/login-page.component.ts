49cdbf38aa386f1dfc673c81c78ebab7c9aa319a
LuckyForce <adrian.schauer@aon.at>
2022-11-23 23:56:25 +0100

    PreviousNavigation

--- projects/user-frontend/src/app/Components/Pages/login-page/login-page.component.ts
+++ projects/user-frontend/src/app/Components/Pages/login-page/login-page.component.ts
@@ -7,18 +7,24 @@ import { UserFrontendRoutes } from '../../../app-routing.module';
 @Component({
   selector: 'user-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
+  constructor(
+    public themeDataService: ThemeDataService,
+    private router: Router
+  ) {}
 
-  constructor(public themeDataService:ThemeDataService, private router:Router) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  public Login(jwt:string){
-    if(jwt){
-      this.router.navigate([UserFrontendRoutes.Home])
+  public Login(jwt: string) {
+    if (jwt) {
+      //Check if there is a return url in the query params
+      if (this.router.getCurrentNavigation()?.previousNavigation != null) {
+        const returnUrl =
+          this.router.getCurrentNavigation()?.previousNavigation;
+        this.router.navigate([returnUrl]);
+      } else this.router.navigate([UserFrontendRoutes.Home]);
     }
   }
 }
