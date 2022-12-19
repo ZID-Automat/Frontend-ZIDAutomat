a218d44c900c0501a640bdfd146db53c19de88fb
LuckyForce <adrian.schauer@aon.at>
2022-12-19 10:40:38 +0100

    QRCode Display

--- projects/user-frontend/src/app/Components/Dialogs/qrcode-dialog/qrcode-dialog.component.ts
+++ projects/user-frontend/src/app/Components/Dialogs/qrcode-dialog/qrcode-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit, NgModule } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Input, OnInit, NgModule, Inject } from '@angular/core';
 
 @Component({
   selector: 'user-qrcode-dialog',
@@ -6,9 +7,20 @@ import { Component, Input, OnInit, NgModule } from '@angular/core';
   styleUrls: ['./qrcode-dialog.component.scss'],
 })
 export class QrcodeDialogComponent implements OnInit {
-  @Input() qrcode: string = undefined!;
 
-  constructor() {}
+  constructor(public dialogRef: MatDialogRef<QrcodeDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: IQRCodeDialogData) {}
 
   ngOnInit(): void {}
+
+  public static openDialog(qrcode: IQRCodeDialogData, dialog: MatDialog) {
+    dialog.open(QrcodeDialogComponent, {
+      data: { qrcode: qrcode.qrcode },
+      width: '80vw',
+      height: '80vh',
+    });
+  }
+}
+
+interface IQRCodeDialogData {
+  qrcode: string;
 }
