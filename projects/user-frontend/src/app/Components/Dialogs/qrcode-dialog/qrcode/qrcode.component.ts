3104058cc6a31f5adc695ec9986250ae78177e86
LuckyForce <adrian.schauer@aon.at>
2023-01-14 23:51:06 +0100

    QR Code Dialog

--- projects/user-frontend/src/app/Components/Dialogs/qrcode-dialog/qrcode/qrcode.component.ts
+++ projects/user-frontend/src/app/Components/Dialogs/qrcode-dialog/qrcode/qrcode.component.ts
@@ -1,4 +1,9 @@
-import { Component, Input, OnInit, NgModule } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  HostListener,
+} from '@angular/core';
 
 @Component({
   selector: 'user-qrcode',
@@ -7,8 +12,29 @@ import { Component, Input, OnInit, NgModule } from '@angular/core';
 })
 export class QrcodeComponent implements OnInit {
   @Input() qrcode: string = undefined!;
+  width: number = 400;
 
-  constructor() {}
+  constructor() {
+    this.updateDays();
+  }
 
   ngOnInit(): void {}
+
+  @HostListener('window:resize', []) updateDays() {
+    // lg (for laptops and desktops - screens equal to or greater than 1200px wide)
+    // md (for small laptops - screens equal to or greater than 992px wide)
+    // sm (for tablets - screens equal to or greater than 768px wide)
+    // xs (for phones - screens less than 768px wide)
+
+    // if (window.innerWidth >= 1200) {
+    //   this.width = 700; // lg
+    // } else if (window.innerWidth >= 992) {
+    //   this.width = 600;//md
+    // } else if (window.innerWidth  >= 768) {
+    //   this.width = 500;//sm
+    // } else if (window.innerWidth < 768) {
+    //   this.width = 300;//xs
+    // }
+    this.width = window.innerWidth * 0.6;
+  }
 }
