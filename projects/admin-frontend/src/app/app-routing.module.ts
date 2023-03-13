3e200e37f1cf12235310a13ed288f19eb3e41069
Maxi1324 <maxi1234567890fischer@gmail.com>
2023-03-13 09:45:38 +0100

    drag and drop start

--- projects/admin-frontend/src/app/app-routing.module.ts
+++ projects/admin-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+import { StatusPageComponent } from './Components/Pages/Status/status-page/status-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './Components/Pages/Login/login-page/login-page.component';
+import { ItemPageComponent } from './Components/Pages/Item/item-page/item-page.component';
 
+export enum AdminFrontendRoutes {
+  Login = 'login',
+  Status="status",
+  Item="items"
+}
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component:LoginPageComponent},
+//  { path: '', redirectTo: AdminFrontendRoutes.Login, pathMatch: 'full' },
+  { path: AdminFrontendRoutes.Login, component:LoginPageComponent},
+  {path:AdminFrontendRoutes.Status, component:StatusPageComponent},
+  {path:AdminFrontendRoutes.Item, component:ItemPageComponent}
 ];
 
 @NgModule({
