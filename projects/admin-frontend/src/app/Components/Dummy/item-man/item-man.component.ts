aec7ca74adaec1435e2197b8df836074b31c103d
LuckyForce <adrian.schauer@aon.at>
2023-03-27 16:22:37 +0200

    #89 - Dialog Creation

--- projects/admin-frontend/src/app/Components/Dummy/item-man/item-man.component.ts
+++ projects/admin-frontend/src/app/Components/Dummy/item-man/item-man.component.ts
@@ -1,18 +1,21 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddItemDialogComponent } from '../../Dialogs/add-item-dialog/add-item-dialog.component';
 
 @Component({
   selector: 'admin-item-man',
   templateUrl: './item-man.component.html',
-  styleUrls: ['./item-man.component.scss']
+  styleUrls: ['./item-man.component.scss'],
 })
 export class ItemManComponent implements OnInit {
+  public h = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 16, 18];
+  @Input() public GrapToPointsId: string[] = [];
 
-  public h = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,16,18]
-  @Input()public GrapToPointsId:string[] = [];
+  constructor(private matDialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openAddItemDialog() {
+    AddItemDialogComponent.openDialog(this.matDialog);
   }
-
 }
--- projects/admin-frontend/src/app/app.module.ts
+++ projects/admin-frontend/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import {
+  MatFormFieldControl,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
 import { LoginPageComponent } from './Components/Pages/Login/login-page/login-page.component';
 import { NavBarComponent } from './Components/Dummy/nav-bar/nav-bar.component';
 import { StatusPageComponent } from './Components/Pages/Status/status-page/status-page.component';
 import { ItemPageComponent } from './Components/Pages/Item/item-page/item-page.component';
 import { AutomatViewComponent } from './Components/Dummy/automat-view/automat-view.component';
 import { ItemManComponent } from './Components/Dummy/item-man/item-man.component';
+import { AddItemDialogComponent } from './Components/Dialogs/add-item-dialog/add-item-dialog.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +27,8 @@ import { ItemManComponent } from './Components/Dummy/item-man/item-man.component
     StatusPageComponent,
     ItemPageComponent,
     AutomatViewComponent,
-    ItemManComponent
+    ItemManComponent,
+    AddItemDialogComponent,
   ],
   imports: [
     AutomatSharedModule,
@@ -31,9 +36,9 @@ import { ItemManComponent } from './Components/Dummy/item-man/item-man.component
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    DragDropModule
+    DragDropModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
