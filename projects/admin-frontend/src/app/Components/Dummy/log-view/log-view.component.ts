441f2c38480bfc8c5c684f4721302ef888a2fb30
LuckyForce <adrian.schauer@aon.at>
2023-04-23 22:55:52 +0200

    #92: First Log Design

--- projects/admin-frontend/src/app/Components/Dummy/log-view/log-view.component.ts
+++ projects/admin-frontend/src/app/Components/Dummy/log-view/log-view.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'admin-log-view',
   templateUrl: './log-view.component.html',
-  styleUrls: ['./log-view.component.scss']
+  styleUrls: ['./log-view.component.scss'],
 })
 export class LogViewComponent implements OnInit {
+  public log_items: LogItem[] = [
+    {
+      timestamp: new Date(),
+      message: 'Test',
+      type: 'info',
+    },
+    {
+      timestamp: new Date(),
+      message: 'Test',
+      type: 'info',
+    },
+    {
+      timestamp: new Date(),
+      message: 'Test',
+      type: 'info',
+    },
+    {
+      timestamp: new Date(),
+      message: 'Test',
+      type: 'info',
+    },
+    {
+      timestamp: new Date(),
+      message: 'Test',
+      type: 'info',
+    },
+    {
+      timestamp: new Date(),
+      message: 'Test',
+      type: 'info',
+    },
+  ];
 
-  constructor() { }
+  // public LoginInfoForm: FormGroup = new FormGroup({
+  //   filter_from_date: new FormControl(
+  //     new Date().toISOString().substring(0, 10)
+  //   ),
+  //   filter_from_time: new FormControl(
+  //     new Date().toISOString().substring(11, 16)
+  //   ),
+  // });
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
+}
+export interface LogItem {
+  timestamp: Date;
+  message: string;
+  type: string;
 }
