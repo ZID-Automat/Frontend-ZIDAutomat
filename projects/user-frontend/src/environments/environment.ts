3ba91d92b5a8a2f8f34ccf5bef2dd2d137f77e2f
Maximilian Fischer <54480672+Maxi1324@users.noreply.github.com>
2023-04-28 09:26:17 +0200

    add Env

--- projects/user-frontend/src/environments/environment.ts
+++ projects/user-frontend/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  backendUrl: "https://10.36.1.202:7141",
 };
 
 /*
