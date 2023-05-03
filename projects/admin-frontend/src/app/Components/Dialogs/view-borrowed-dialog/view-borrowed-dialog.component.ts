d42fbf3e151f5b6851fbadad370116eb5d32ab60
LuckyForce <adrian.schauer@aon.at>
2023-05-03 12:21:05 +0200

    #102: Dialog Working

--- projects/admin-frontend/src/app/Components/Dialogs/view-borrowed-dialog/view-borrowed-dialog.component.ts
+++ projects/admin-frontend/src/app/Components/Dialogs/view-borrowed-dialog/view-borrowed-dialog.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { BorrowedItem } from '../../Dummy/borrowed-view/borrowed-view.component';
 
 @Component({
@@ -8,13 +12,16 @@ import { BorrowedItem } from '../../Dummy/borrowed-view/borrowed-view.component'
   styleUrls: ['./view-borrowed-dialog.component.scss'],
 })
 export class ViewBorrowedDialogComponent implements OnInit {
-  constructor(public dialogRef: MatDialogRef<ViewBorrowedDialogComponent>) {}
+  constructor(
+    public dialogRef: MatDialogRef<ViewBorrowedDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: BorrowedItem
+  ) {}
 
   ngOnInit(): void {}
 
   public static openDialog(dialog: MatDialog, borrowed: BorrowedItem) {
     dialog.open(ViewBorrowedDialogComponent, {
-      // data: { qrcode: qrcode.qrcode },
+      data: borrowed,
       width: '80%',
       height: 'fit-content',
       maxWidth: '600px',
