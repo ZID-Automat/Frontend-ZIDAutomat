a06cbc272e6d17c3bbef4ea8c99455b729a9505b
Maximilian Fischer <54480672+Maxi1324@users.noreply.github.com>
2023-05-31 20:00:09 +0200

    color change on hove user detailed

--- projects/admin-frontend/src/app/Components/Dialogs/user-detailed-dialog/user-detailed-dialog.component.ts
+++ projects/admin-frontend/src/app/Components/Dialogs/user-detailed-dialog/user-detailed-dialog.component.ts
@@ -1,32 +1,55 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 import { ColumnMode } from '@swimlane/ngx-datatable';
-import {AUserInforService,UserAdminDetailedDto} from 'AutomatApi';
+import { AUserInforService, UserAdminDetailedDto } from 'AutomatApi';
 @Component({
   selector: 'admin-user-detailed-dialog',
   templateUrl: './user-detailed-dialog.component.html',
-  styleUrls: ['./user-detailed-dialog.component.scss']
+  styleUrls: ['./user-detailed-dialog.component.scss'],
 })
 export class UserDetailedDialogComponent implements OnInit {
-
-  public user:UserAdminDetailedDto = null!
-  public ColumnMode = ColumnMode
-
-  constructor(private AUserInforService:AUserInforService,public dialogRef: MatDialogRef<UserDetailedDialogComponent> , @Inject(MAT_DIALOG_DATA) public data: any,) { }
+  public user: UserAdminDetailedDto = null!;
+  public ColumnMode = ColumnMode;
+  constructor(
+    private AUserInforService: AUserInforService,
+    public dialogRef: MatDialogRef<UserDetailedDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
   ngOnInit(): void {
-    this.AUserInforService.aUserInforDetailedUserGet$Json({id:this.data.id}).subscribe((data) => {
+    this.AUserInforService.aUserInforDetailedUserGet$Json({
+      id: this.data.id,
+    }).subscribe((data) => {
       this.user = data;
-    })
+    });
   }
-  
-  public static openDialog(dialog: MatDialog, id:number) {
+
+  public static openDialog(dialog: MatDialog, id: number) {
     dialog.open(UserDetailedDialogComponent, {
-       data: { id: id },
+      data: { id: id },
       width: '70%',
       height: '80vh',
     });
   }
 
-  columns = [{ name: 'Id' }, { name: 'BorrowDate' },{ name: 'Returned' },{ name: 'Itemname' }];
+  public getRowClass(row: any) {
+    let stat =row.stati
+    if(stat == 0){
+      return 'borrowNotReturned';
+    }
+    if(stat == 2){
+      return 'borrowReturned';
+    }
+    return "";
+  }
+
+  columns = [
+    { name: 'Id' },
+    { name: 'Itemname' },
+    { name: 'BorrowDate' },
+  ];
 }
--- projects/automat-api/src/API/models/user-admi-borrow-dto.ts
+++ projects/automat-api/src/API/models/user-admi-borrow-dto.ts
@@ -4,5 +4,5 @@ export interface UserAdmiBorrowDto {
   borrowDate?: string;
   id?: number;
   itemname?: null | string;
-  returned?: boolean;
+  stati?: number;
 }
