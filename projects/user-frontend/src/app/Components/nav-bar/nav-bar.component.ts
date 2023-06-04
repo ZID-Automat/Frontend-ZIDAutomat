d4e1a26fbb7273701c1c66499128387687bb5f01
Maximilian Fischer <54480672+Maxi1324@users.noreply.github.com>
2023-06-04 19:51:26 +0200

    nav bar fix1

--- projects/user-frontend/src/app/Components/nav-bar/nav-bar.component.ts
+++ projects/user-frontend/src/app/Components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,6 @@
 import { QrCodeDataService } from './../../Services/DataServices/qrCodeData.service';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { ThemeDataService } from 'AutomatShared';
 import { UserFrontendRoutes } from '../../app-routing.module';
 import { take } from 'rxjs';
@@ -10,13 +10,24 @@ import { take } from 'rxjs';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements OnInit, OnDestroy {
 
   public UserFrontendRoutes:any = UserFrontendRoutes
 
   count = 0
+  alive = true
 
   constructor(public themeDataService:ThemeDataService,private router:Router, private qrCodeDataService:QrCodeDataService) { }
+  ngOnDestroy(): void {
+    this.alive = false
+
+    this.router.events.subscribe(event => {
+      console.log(event)
+      if (event instanceof NavigationEnd) {
+        console.log('URL changed:', event.url);
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.qrCodeDataService.activeQrCodesCount().pipe(take(1)).subscribe(res => this.count = res)
@@ -24,5 +35,10 @@ export class NavBarComponent implements OnInit {
 
   route(route:UserFrontendRoutes){
     this.router.navigate([route])
+  }
+
+  logout(){
+    localStorage.removeItem('jwt')
+    this.router.navigate([UserFrontendRoutes.Login])
   }
 }
