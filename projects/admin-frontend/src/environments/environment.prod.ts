2d0394745034d9afb7eba4cae45137e3b6d80c95
[SPG] PUW <117909782+spg-puw@users.noreply.github.com>
2023-06-23 12:17:08 +0200

    update backend

--- projects/admin-frontend/src/environments/environment.prod.ts
+++ projects/admin-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: "http://localhost:801",
+  backendUrl: "https://innolab.spengergasse.at/automat/backend",
   stockimagekey:"" //https://www.pexels.com/
 };
